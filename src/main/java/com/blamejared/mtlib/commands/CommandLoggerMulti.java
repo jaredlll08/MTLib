8f915f8dcaa69f6f33668f3a43d42c5e5e7eaf42
Jared <jaredlll08@gmail.com>
2016-10-27 21:15:23 +0200

    updated CommandLoggerMulti

--- src/main/java/com/blamejared/mtlib/commands/CommandLoggerMulti.java
+++ src/main/java/com/blamejared/mtlib/commands/CommandLoggerMulti.java
@@ -14,13 +14,15 @@ public abstract class CommandLoggerMulti implements ICommandFunction {
     @Override
     public void execute(String[] arguments, IPlayer player) {
         List<String> args = StringHelper.toLowerCase(Arrays.asList(arguments));
-
         if(!getLists().keySet().containsAll(args)) {
             if(player != null) {
                 player.sendChat(MineTweakerImplementationAPI.platform.getMessage("Invalid arguments for command. Valid arguments: " + StringHelper.join(getLists().keySet(), ", ")));
             }
         } else {
             getLists().get(args.get(0)).execute(arguments, player);
+            if(player != null) {
+                player.sendChat(MineTweakerImplementationAPI.platform.getMessage("List generated; see minetweaker.log in your minecraft dir"));
+            }
         }
     }
 
