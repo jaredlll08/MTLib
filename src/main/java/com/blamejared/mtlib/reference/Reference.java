8dd5f7abfca7250518b898387fdd900ff160ed37
Jaredlll08 <jaredlll08@gmail.com>
2017-05-27 21:08:22 +0200

    added loading stages

--- src/main/java/com/blamejared/mtlib/reference/Reference.java
+++ src/main/java/com/blamejared/mtlib/reference/Reference.java
@@ -1,5 +1,7 @@
 package com.blamejared.mtlib.reference;
 
+import java.io.File;
+
 /**
  * Created by Jared.
  */
@@ -8,5 +10,9 @@ public class Reference {
     public static final String MOD_ID = "mtlib";
     public static final String MOD_NAME = "MTLib";
     public static final String VERSION = "@VERSION@";
-
+    public static File scriptDirBase;
+    public static File scriptDirPreInit;
+    public static File scriptDirInit;
+    public static File scriptDirPostInit;
+    
 }
