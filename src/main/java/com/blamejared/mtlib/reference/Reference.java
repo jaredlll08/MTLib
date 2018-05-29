f10953781efd34e0506669b8997f296d60e73924
Jared <jaredlll08@gmail.com>
2018-05-29 13:38:03 +0200

    version push

--- src/main/java/com/blamejared/mtlib/reference/Reference.java
+++ src/main/java/com/blamejared/mtlib/reference/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
     
     public static final String MOD_ID = "mtlib";
     public static final String MOD_NAME = "MTLib";
-    public static final String VERSION = "3.0.4";
+    public static final String VERSION = "3.0.5";
     
 }
